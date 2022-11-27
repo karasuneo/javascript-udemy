74b20f7515b91a12e27145bcaddf0512a0e4dd3e
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-27 09:12:48 +0900

    変数の定義について

---
+++ src/index.js
@@ -0,0 +1,39 @@
+// var val1 = "var変数";
+// console.log(val1);
+
+// // varは上書き可能
+// val1 = "var変数を上書き";
+// console.log(val1);
+
+// // varは再宣言可能
+// var var1 = "var変数を再宣言";
+// console.log(val1);
+
+// let var2 = "lete変数"
+// console.log(var2)
+
+// // letは上書き可能
+// var2 = "let変数を上書き"
+// console.log(var2)
+
+// // letは再宣言不可
+// let var2 = "let変数を上書き"
+
+// const var3 = "const変数"
+// console.log(var3)
+
+// // const変数は上書き不可
+// var3 = "const変数を上書き"
+
+// // const変数は再宣言不可
+// const var3 = "const変数を再宣言"
+// console.log(var3)
+
+// const で定義したオブジェクトは変更可能
+// const val4 = {
+//   name: "mike",
+//   age: 28,
+// };
+// val4.name = "aaa";
+// val4.adress = "nagoya";
+// console.log(val4);
