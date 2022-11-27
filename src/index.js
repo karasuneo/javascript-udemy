141050f68cc984f8029bda579e4cab515b10c83c
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-27 10:34:41 +0900

    スプレッド構文

--- src/index.js
+++ src/index.js
@@ -37,3 +37,66 @@
 // val4.name = "aaa";
 // val4.adress = "nagoya";
 // console.log(val4);
+
+// // 従来の関数
+// function func1(str) {
+//   return str;
+// }
+// console.log(func1("funcだよ"));
+
+// //アロー関数
+// const func2 = (str) => {
+//   return str;
+// };
+// console.log(func2("func2です"));
+
+// const func3 = (num1, num2) => {
+//   return num1 + num2;
+// };
+// console.log(func3(10, 20))
+
+// // 分割代入
+// const myProfille = {
+//       name: "jyon",
+//       age: 28
+// }
+
+// const messagge1 = `名前は${myProfille.name}です。年齢は${myProfille.age}歳です。`
+// console.log(messagge1)
+
+// const {name, age} = myProfille
+// const messagge2 = `名前は${name}です。年齢は${age}歳です。`
+// console.log(messagge2)
+
+// undefinedにならないようにする
+// const sayHello = (name = "aaa") => console.log(`こんにちは${name}さん！`);
+// sayHello();
+
+// // スプレッド構文
+// const arr1 = [1, 2];
+// console.log(arr1);
+// console.log(...arr1);
+
+// const sumFunc = (num1, num2) => console.log(num1, num2);
+// sumFunc(arr1[0], arr1[1]);
+// sumFunc(...arr1);
+
+// // まとめる
+// const arr2 = [1, 2, 3, 4, 5];
+// const [num1, num2, ...arr3] = arr2;
+// console.log(num1);
+// console.log(num2);
+// console.log(arr3);
+
+// // 配列の結合、コピー
+// const arr4 = [10, 20];
+// const arr5 = [30, 40];
+
+// const arr6 = [...arr4];
+// console.log(arr6);
+
+// const arr7 = [...arr4, ...arr5];
+// console.log(arr7);
+
+// // =で繋ぐと参照先のコピーになるため、よくない
+
