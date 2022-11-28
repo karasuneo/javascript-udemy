a192f9a88b923b5d4d685d30d370524f8696cae5
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-28 11:00:08 +0900

    tキスとボックスの追加

--- todo-js/src/index.js
+++ todo-js/src/index.js
@@ -1,7 +1,21 @@
 const onClickAdd = () => {
+  // テキストボックスの値を取得し、初期化
   const inputText = document.getElementById("add-text").value;
-  document.getElementById("add-text").value = ""
-  alert(inputText)
+  document.getElementById("add-text").value = "";
+  //   div生成
+  const div = document.createElement("div");
+  div.className = "list-row";
+
+  // liタグ生成
+  const li = document.createElement("li");
+  li.innerText = inputText
+
+  // duvタグの子要素に各要素を設定
+  div.appendChild(li)
+
+  // 未完了のリストに追加
+  document.getElementById("incpomplete-list").appendChild(div)
+  alert(inputText);
 };
 
 document
