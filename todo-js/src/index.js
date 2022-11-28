96227af63b9e7c9b4cc369c5ed528ae8ed5c7c2f
Yusei Mizutani <yusei4520030401@gmail.com>
2022-11-28 12:31:47 +0900

    コード成形

--- todo-js/src/index.js
+++ todo-js/src/index.js
@@ -8,13 +8,13 @@ const onClickAdd = () => {
 
   // liタグ生成
   const li = document.createElement("li");
-  li.innerText = inputText
+  li.innerText = inputText;
 
   // duvタグの子要素に各要素を設定
-  div.appendChild(li)
+  div.appendChild(li);
 
   // 未完了のリストに追加
-  document.getElementById("incpomplete-list").appendChild(div)
+  document.getElementById("incpomplete-list").appendChild(div);
   alert(inputText);
 };
 
