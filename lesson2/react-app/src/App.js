f772f0850aec51eae168d726390bbe9ef2ac5faa
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-05 12:40:38 +0900

    レンダリング確認

--- lesson2/react-app/src/App.js
+++ lesson2/react-app/src/App.js
@@ -1,18 +1,26 @@
 import { useState } from "react";
+import { ChildArea } from "./ChildArea";
 import "./App.css";
 
 export default function App() {
   const [count, setCount] = useState(0);
-  
+  const [text, setText] = useState("");
+  const [open, setOpen] = useState(false);
+
   const onClickCountUp = () => {
     setCount(count + 1);
   };
-  
+
+  const onChangeText = (e) => setText(e.target.value);
+
+  const onClickOpen = () => setOpen(!open)
   return (
     <div className="App">
-      <h1>Hello</h1>
-      <p>{count}</p>
-      <button onClick={onClickCountUp}>カウントアップ</button>
+      <input value={text} onChange={onChangeText}/>
+      <br />
+      <br />
+      <button onClick={onClickOpen}>表示</button>
+      <ChildArea open={open}/>
     </div>
   );
 }
