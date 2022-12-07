25440a8b4828e27534a56d86cff661949c76fa35
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-07 21:21:03 +0900

    react追加

---
+++ lesson2/react-css/src/App.js
@@ -0,0 +1,12 @@
+
+import { InlineStyle } from './components/inlinestyle'
+
+function App() {
+  return (
+    <div className="App">
+      <InlineStyle />
+    </div>
+  );
+}
+
+export default App;
