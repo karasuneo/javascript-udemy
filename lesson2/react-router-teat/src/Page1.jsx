9701495fac77f617606ba55ca621374950bfe23a
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-11 12:44:53 +0900

    React-routerについて

--- lesson2/react-router-teat/src/Page1.jsx
+++ lesson2/react-router-teat/src/Page1.jsx
@@ -1,12 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useHistory } from "react-router-dom";
 
 export const Page1 = () => {
+  const arr = [...Array(100).keys()]
+  console.log(arr)
+
+  const history = useHistory()
+
+  const onClickHistory = () => history.push("/page1/detail1")
   return (
     <div>
       <h1>Page1です</h1>
-      <Link to="/page1/detail1">Detail1</Link>
+      <Link to={{ pathname: "/page1/detail1", state: arr }}>Detail1</Link>
       <br />
       <Link to="/page1/detail2">Detail2</Link>
+      <br />
+      <button onClick={onClickHistory}>Detial1</button>
     </div>
   );
 };
