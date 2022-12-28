f4cf572b77a11dbfcb0f035c594a24564063a3d3
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-28 23:30:59 +0900

    カスタムフック

---
+++ lesson2/react-custom/src/App.tsx
@@ -0,0 +1,43 @@
+import "./App.css";
+import { UserCard } from "./components/UserCard";
+import axios from "axios";
+import { User } from "./types/api/user";
+import { useState } from "react";
+import { UserProfile } from "./types/userProfile";
+
+const user = {
+  id: 3,
+  name: "John",
+  email: "3245678@cc.ac.jp",
+  address: "foijdwfj",
+};
+
+function App() {
+  const [userProfiles, setUserProfiles] = useState<Array<UserProfile>>([]);
+
+  
+
+  const onClickFetchUser = () => {
+    axios
+      .get<Array<User>>("https://jsonplaceholder.typicode.com/users")
+      .then((res) => {
+        const data = res.data.map((user) => ({
+          id: user.id,
+          name: `${user.name}(${user.username})`,
+          email: user.email,
+          address: `${user.address.city}${user.address.suite}${user.address.street}`,
+        }));
+        setUserProfiles(data);
+      });
+  };
+  return (
+    <div className="App">
+      <button onClick={onClickFetchUser}>データ取得</button>
+      {userProfiles.map((user) => (
+        <UserCard key={user.id} user={user} />
+      ))}
+    </div>
+  );
+}
+
+export default App;
