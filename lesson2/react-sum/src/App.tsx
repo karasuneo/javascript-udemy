2171394de6dbdd3f372f7c23b8bde7884dee0c15
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-29 23:55:48 +0900

    まとめさいと

---
+++ lesson2/react-sum/src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css';
+import { Button, ChakraProvider } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <ChakraProvider>
+      <Button>vsdv</Button>
+    </ChakraProvider>
+  );
+}
+
+export default App;
