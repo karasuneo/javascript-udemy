47caf52e3ac28f317c8da1873b1d5d6f1d5277f7
Yusei Mizutani <yusei4520030401@gmail.com>
2022-12-31 20:45:04 +0900

    ヘッダの作成

--- lesson2/react-sum/src/components/organisms/layout/Header.tsx
+++ lesson2/react-sum/src/components/organisms/layout/Header.tsx
@@ -1,9 +1,69 @@
 import { FC, memo } from "react";
+import {
+  Flex,
+  Box,
+  Heading,
+  Link,
+  IconButton,
+  Drawer,
+  DrawerOverlay,
+  DrawerContent,
+  DrawerBody,
+} from "@chakra-ui/react";
+import { HamburgerIcon } from "@chakra-ui/icons";
+import { Button, useDisclosure } from "@chakra-ui/react";
 
 export const Header: FC = memo(() => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+
   return (
-    <div style={{ height: "50px", backgroundColor: "teal" }}>
-      Homeページです
-    </div>
+    <>
+      <Flex
+        as="nav"
+        bg="teal.500"
+        color="gray.50"
+        align="center"
+        justify="space-between"
+        padding={{ base: 3, md: 5 }}
+      >
+        <Flex align="center" as="a" mr={8} _hover={{ cursor: "pointer" }}>
+          <Heading as="h1" fontSize={{ base: "md", md: "lg" }}>
+            ユーザ管理アプリ
+          </Heading>
+        </Flex>
+
+        <Flex
+          align="center"
+          fontSize="sm"
+          flexGrow={2}
+          display={{ base: "none", md: "flex" }}
+        >
+          <Box pr={4}>
+            <Link>ユーザ一覧</Link>
+          </Box>
+
+          <Link>設定</Link>
+        </Flex>
+        <IconButton
+          aria-label="メニューボタン"
+          icon={<HamburgerIcon />}
+          size="sm"
+          variant="unstyled"
+          onClick={onOpen}
+          display={{ base: "black", md: "none" }}
+        />
+      </Flex>
+      <Drawer placement="left" size="xs" onClose={onClose} isOpen={isOpen}>
+        <DrawerOverlay>
+          <DrawerContent>
+            <DrawerBody p={0} bg="gray.100">
+              <Button w="100%">TOP</Button>
+              <Button w="100%">ユーザ一覧</Button>
+              <Button w="100%">設定</Button>
+            </DrawerBody>
+          </DrawerContent>
+        </DrawerOverlay>
+      </Drawer>
+    </>
   );
 });
