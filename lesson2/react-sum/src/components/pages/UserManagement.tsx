33d9773be2bf4f99290d8ba2cb57458ad1facad3
Yusei Mizutani <yusei4520030401@gmail.com>
2023-01-01 22:27:38 +0900

    選択したユーザのモーダルを表示

--- lesson2/react-sum/src/components/pages/UserManagement.tsx
+++ lesson2/react-sum/src/components/pages/UserManagement.tsx
@@ -1,11 +1,27 @@
-import { memo, FC, useEffect } from "react";
-import { Wrap, WrapItem, Spinner, Center } from "@chakra-ui/react";
+import { memo, FC, useEffect, useCallback } from "react";
+import {
+  Wrap,
+  WrapItem,
+  Spinner,
+  Center,
+  useDisclosure,
+} from "@chakra-ui/react";
 
-import { UserCard } from "../molecules/user/UserCard";
+import { UserCard } from "../organisms/user/UserCard";
 import { useAllUsers } from "../../hooks/useAllUsers";
+import { UserDetailModal } from "../organisms/user/UserDetailModal";
+import { useSelectUser } from "../../hooks/useSelectUser";
 
 export const UserManagement: FC = memo(() => {
   const { getUsers, users, loading } = useAllUsers();
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onSelectUser, selectUser } = useSelectUser();
+  console.log(selectUser);
+
+  const onClickUser = useCallback((id: number) => {
+    onSelectUser({ id, users, onOpen });
+    onOpen();
+  }, [users, onSelectUser, onOpen]);
 
   useEffect(() => getUsers(), []);
 
@@ -20,14 +36,17 @@ export const UserManagement: FC = memo(() => {
           {users.map((user) => (
             <WrapItem key={user.id} mx="auto">
               <UserCard
+                id={user.id}
                 imageUrl="https://source.unsplash.com/random"
                 userName={user.username}
                 fullName={user.name}
+                onClick={onClickUser}
               />
             </WrapItem>
           ))}
         </Wrap>
       )}
+      <UserDetailModal user={selectUser} isOpen={isOpen} onClose={onClose} />
     </>
   );
 });
