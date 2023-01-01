7cf46c4f07a48ca76912128bf33ada6111765a50
Yusei Mizutani <yusei4520030401@gmail.com>
2023-01-01 14:36:57 +0900

    ぉグイン機能

--- lesson2/react-sum/src/components/pages/Login.tsx
+++ lesson2/react-sum/src/components/pages/Login.tsx
@@ -1,16 +1,18 @@
-import { memo, FC } from "react";
-import {
-  Flex,
-  Heading,
-  Divider,
-  Input,
-  Button,
-  Box,
-  Stack,
-} from "@chakra-ui/react";
+import { memo, FC, useState, ChangeEvent } from "react";
+import { Flex, Heading, Divider, Input, Box, Stack } from "@chakra-ui/react";
 import { PrimaryButton } from "../atoms/button/PrimaryButton";
+import { useAuth } from "../../hooks/useAuth";
 
 export const Login: FC = memo(() => {
+  const { login, loading } = useAuth();
+
+  const [userId, setUserId] = useState("");
+
+  const onChangeUserId = (e: ChangeEvent<HTMLInputElement>) =>
+    setUserId(e.target.value);
+
+  const onClickLogin = () => login(userId);
+
   return (
     <Flex align="center" justify="center" height="100vh">
       <Box bg="white" w="sm" p={4} borderRadius="md" shadow="md">
@@ -19,8 +21,18 @@ export const Login: FC = memo(() => {
         </Heading>
         <Divider my={4} />
         <Stack spacing={6} py={4} px={10}>
-          <Input placeholder="ユーザID" />
-          <PrimaryButton>ログイン</PrimaryButton>
+          <Input
+            placeholder="ユーザID"
+            value={userId}
+            onChange={onChangeUserId}
+          />
+          <PrimaryButton
+            disabled={userId === ""}
+            loading={loading}
+            onClick={onClickLogin}
+          >
+            ログイン
+          </PrimaryButton>
         </Stack>
       </Box>
     </Flex>
